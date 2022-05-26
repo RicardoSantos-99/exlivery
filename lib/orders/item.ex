877d818345c8c50c1dcb5ea9f26884169713a994
Ricardo Carvalho Santos <ricardo2010carvalho@hotmail.com>
2022-05-26 16:09:36 -0300

    feat: add item and order struct

---
+++ lib/orders/item.ex
@@ -0,0 +1,24 @@
+defmodule Exlivery.Orders.Item do
+  @categories [:pizza, :hamburger, :carne, :prato_feito, :japonesa, :sobremesa]
+
+  @keys [:description, :category, :unit_price, :quantity]
+
+  @enforce_keys @keys
+
+  defstruct @keys
+
+  def build(description, category, unit_price, quantity)
+      when quantity > 0 and category in @categories do
+    {:ok,
+     %__MODULE__{
+       description: description,
+       category: category,
+       unit_price: unit_price,
+       quantity: quantity
+     }}
+  end
+
+  def build(_description, _quantity, _category, _unit_price) do
+    {:error, "invalid parameters"}
+  end
+end

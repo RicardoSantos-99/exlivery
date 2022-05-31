93a31c2da2112ba4a7ba7652fec340eb97095baa
Ricardo Carvalho Santos <ricardo2010carvalho@hotmail.com>
2022-05-31 14:22:41 -0300

    test: add order factory

--- test/support/factory.ex
+++ test/support/factory.ex
@@ -2,7 +2,7 @@ defmodule Exlivery.Factory do
   use ExMachina
 
   alias Exlivery.Users.User
-  alias Exlivery.Orders.Item
+  alias Exlivery.Orders.{Order, Item}
 
   def user_factory do
     %User{
@@ -20,6 +20,23 @@ defmodule Exlivery.Factory do
       category: :pizza,
       quantity: 1,
       unity_price: Decimal.new("35.5")
+    }
+  end
+
+  def order_factory do
+    %Order{
+      delivery_address: "Rua dos bobos",
+      items: [
+        build(:item),
+        build(:item,
+          description: "Temaki",
+          category: :japonesa,
+          unity_price: Decimal.new("30.50"),
+          quantity: 2
+        )
+      ],
+      total_price: Decimal.new("96.50"),
+      user_cpf: "12345678901"
     }
   end
 end

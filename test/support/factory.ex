47bf7fef854486fafccfd0bda3070ad11a5ef0c1
Ricardo Carvalho Santos <ricardo2010carvalho@hotmail.com>
2022-05-30 22:38:29 -0300

    test: create item factory

--- test/support/factory.ex
+++ test/support/factory.ex
@@ -1,6 +1,8 @@
 defmodule Exlivery.Factory do
   use ExMachina
+
   alias Exlivery.Users.User
+  alias Exlivery.Orders.Item
 
   def user_factory do
     %User{
@@ -9,6 +11,15 @@ defmodule Exlivery.Factory do
       email: "bob@bobs.com",
       cpf: "12345678901",
       age: 27
+    }
+  end
+
+  def item_factory do
+    %Item{
+      description: "Pizza",
+      category: :pizza,
+      quantity: 1,
+      unity_price: Decimal.new("35.5")
     }
   end
 end

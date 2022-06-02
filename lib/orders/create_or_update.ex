902111aa7fda57afa84834a5d073ea5ba554ceae
Ricardo Carvalho Santos <ricardo2010carvalho@hotmail.com>
2022-06-02 10:12:07 -0300

    feat: add create or update order

--- lib/orders/create_or_update.ex
+++ lib/orders/create_or_update.ex
@@ -1,8 +1,7 @@
 defmodule Exlivery.Orders.CreateOrUpdate do
-  alias Exlivery.Users.Order, as: OrderAgent
-  alias Exlivery.Orders.Agent, as: UserAgent
-
-  alias Exlivery.Orders.Order
+  alias Exlivery.Orders.{Order, Item}
+  alias Exlivery.Users.Agent, as: UserAgent
+  alias Exlivery.Orders.Agent, as: OrderAgent
 
   def call(%{user_cpf: user_cpf, items: items}) do
     with {:ok, user} <- UserAgent.get(user_cpf),
@@ -14,6 +13,25 @@ defmodule Exlivery.Orders.CreateOrUpdate do
     end
   end
 
-  defp build_items(%{items: items}) do
+  defp build_items(items) do
+    items
+    |> Enum.map(&build_item/1)
+    |> handle_build()
+  end
+
+  defp build_item(%{
+         description: description,
+         category: category,
+         unity_price: unity_price,
+         quantity: quantity
+       }) do
+    case Item.build(description, category, unity_price, quantity) do
+      {:ok, item} -> item
+      {:error, _reason} = error -> error
+    end
+  end
+
+  defp handle_build(items) do
+    if Enum.all?(items, &is_struct/1), do: {:ok, items}, else: {:errror, "invalid items"}
   end
 end

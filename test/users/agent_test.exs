232f324223819ebb3d3191d04fe2730616dd6c63
Ricardo Carvalho Santos <ricardo2010carvalho@hotmail.com>
2022-06-02 12:09:34 -0300

    test: add agent test

---
+++ test/users/agent_test.exs
@@ -0,0 +1,17 @@
+defmodule Exlivery.Users.AgentTest do
+  use ExUnit.Case
+
+  alias Exlivery.Users.Agent, as: UserAgent
+
+  import Exlivery.Factory
+
+  describe "save/1" do
+    test "saves the user" do
+      user = build(:user)
+
+      UserAgent.start_link(%{})
+
+      assert UserAgent.save(user) == :ok
+    end
+  end
+end

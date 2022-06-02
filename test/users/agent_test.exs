7cd5fb6afb44954b62f0c057ed33a2b195e7c21a
Ricardo Carvalho Santos <ricardo2010carvalho@hotmail.com>
2022-06-02 12:19:30 -0300

    feat: add test for get/1 in agent

--- test/users/agent_test.exs
+++ test/users/agent_test.exs
@@ -2,6 +2,7 @@ defmodule Exlivery.Users.AgentTest do
   use ExUnit.Case
 
   alias Exlivery.Users.Agent, as: UserAgent
+  alias Exlivery.Users.User
 
   import Exlivery.Factory
 
@@ -12,6 +13,48 @@ defmodule Exlivery.Users.AgentTest do
       UserAgent.start_link(%{})
 
       assert UserAgent.save(user) == :ok
+    end
+  end
+
+  describe "get/1" do
+    setup do
+      UserAgent.start_link(%{})
+
+      cpf = "12345678901"
+
+      {:ok, cpf: cpf}
+    end
+
+    test "when the user is found, returns the user", %{cpf: cpf} do
+      :user
+      |> build(cpf: cpf)
+      |> UserAgent.save()
+
+      response = UserAgent.get(cpf)
+
+      expected_response =
+        {:ok,
+         %User{
+           address: "Rua dos bobos",
+           age: 27,
+           cpf: "12345678901",
+           email: "bob@bobs.com",
+           name: "Bob"
+         }}
+
+      assert response == expected_response
+    end
+
+    test "when the user is not found, returns an error", %{cpf: cpf} do
+      :user
+      |> build(cpf: cpf)
+      |> UserAgent.save()
+
+      response = UserAgent.get("000000000")
+
+      expected_response = {:error, "user not found"}
+
+      assert response == expected_response
     end
   end
 end
